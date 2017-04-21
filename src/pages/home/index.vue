ba48c8f45283b36008565e58e86ed8b9ec0bcece
yummy1121@foxmail.com <yummy1121@foxmail.com>
2017-04-21 15:11:34 +0800

    修改form的input和button样式

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -3,8 +3,8 @@
     <v-header title="Your First Vue.js App"></v-header>
     <div class="container">
       <div class="form">
-        <input v-model="comment" placeholder="post your comment" @keyup.enter="addComment" />
-        <button @click="addComment">submit</button>
+        <input class="comment-input" v-model="comment" placeholder="post your comment" @keyup.enter="addComment" />
+        <a @click="addComment" class="submit">submit</a>
         <span class="tip">Tips：you can press enter to submit!</span>
       </div>
       <v-comment :list="list"></v-comment>
@@ -15,7 +15,7 @@
 <script>
 
 import axios from 'axios'
-import { timeFormat } from '../../utils' 
+import { timeFormat } from '../../utils'
 
 export default {
   data() {
@@ -45,9 +45,9 @@ export default {
   },
   created: function () {
     axios.get('api/project/comments.action', {})
-    .then((res) => {
-      this.list = res.data.comments
-    })
+      .then((res) => {
+        this.list = res.data.comments
+      })
   }
 }
 </script>
@@ -61,7 +61,39 @@ export default {
   padding-bottom: 15px;
   border-bottom: 1px dashed #dddddd;
 }
+.comment-input {
+  width: 200px;
+  height: 30px;
+  line-height: 30px;
+  box-sizing: border-box;
+  padding: 0 15px;
+  border: 1px solid #e3e3e3;
+  color: #2c3e50;
+  outline: none;
+  border-radius: 15px;
+  margin-right: 10px;
+  transition: border-color 0.2s ease;
+  background-size: 20px;
+  vertical-align: middle !important;
+}
+.comment-input:focus {
+  border-color: #42b983;
+}
+.submit {
+  padding: 3px 20px;
+  border-radius: 15px;
+  display: inline-block;
+  color: #fff;
+  background-color: #4fc08d;
+  transition: all 0.15s ease;
+  box-sizing: border-box;
+  border: 1px solid #4fc08d;
+}
+.submit:hover {
+  cursor: pointer;
+}
 .tip {
+  padding-left: 10px;
   color: #42b983;
   font-size: 13px;
 }
